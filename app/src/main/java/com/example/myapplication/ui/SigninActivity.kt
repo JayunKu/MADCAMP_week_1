32dd56c2e0b1170a85ce8abb15f9a68ebbe35601
us2xince <kstar1029@ewhain.net>
2025-07-05 16:37:41 +0900

    입력란 style,Signin Signup ui 구현

---
+++ app/src/main/java/com/example/myapplication/ui/SigninActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.ui
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class SigninActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_signin)
+    }
+
+}
