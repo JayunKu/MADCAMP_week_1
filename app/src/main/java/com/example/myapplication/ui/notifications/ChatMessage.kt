cce039bbe3079b99712ae796cd4d434ce1f52761
JayunKu <jayun0715@gmail.com>
2025-07-07 20:48:03 +0900

    basic chat func

--- app/src/main/java/com/example/myapplication/ui/notifications/ChatMessage.kt
+++ app/src/main/java/com/example/myapplication/ui/notifications/ChatMessage.kt
@@ -1,6 +1,13 @@
 package com.example.myapplication.ui.notifications
 
+//data class ChatMessage(
+//    val message: String = "",
+//    val isUser: Boolean = false // true: 나, false: 상대방
+//)
+
 data class ChatMessage(
-    val message: String,
-    val isUser: Boolean // true: 나, false: 상대방
+    val message: String = "",
+    val senderId: String = "",
+    val receiverId: String = "",
+    val timestamp: Long = System.currentTimeMillis()
 )
